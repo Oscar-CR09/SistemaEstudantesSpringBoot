a9d45675a3d7c67c4162fd54a0c1919ffcbb227b
Oscar <oscarcr189@outlook.com>
2023-10-13 20:27:38 -0600

    Sistema EstudiantesSpringBoot-1

---
+++ src/test/java/example/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package example.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
