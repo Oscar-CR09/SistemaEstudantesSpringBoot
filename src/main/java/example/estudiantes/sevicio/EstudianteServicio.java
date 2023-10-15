59d96dbd1646a095e209c80702a629f43a9b2b7f
Oscar <oscarcr189@outlook.com>
2023-10-14 19:28:52 -0600

    Sistema EstudiantesSpringBoot-6 capa servicio

--- src/main/java/example/estudiantes/sevicio/EstudianteServicio.java
+++ src/main/java/example/estudiantes/sevicio/EstudianteServicio.java
@@ -1,5 +1,43 @@
 package example.estudiantes.sevicio;
 
+import example.estudiantes.modelo.Estudiante;
+import example.estudiantes.repositorio.EstudianteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EstudianteServicio implements IEstudianteServicio{
-    
+
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiante() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
+
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+
+
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+
+        estudianteRepositorio.delete(estudiante);
+        
+    }
 }
